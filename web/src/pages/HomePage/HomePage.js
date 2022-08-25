c2e435ad107b91d632887d63606c7a5a424886f7
moorels <sales@micropls.com>
2022-08-25 21:54:47 +1000

    first commit

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>
+        etster
+      </p>
+    </>
+  )
+}
+
+export default HomePage
